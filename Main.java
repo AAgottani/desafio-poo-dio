b1d78eb7d277b63a0838a30873edc21c0699c012
AAgottani <adilson.reis@pucpr.edu.br>
2022-07-21 11:26:20 -0300

    Final

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.time.LocalDate;
 
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.Bootcamp;
 
 public class Main {
 
@@ -26,9 +28,29 @@ public class Main {
 	mentoria.setDescricao("Descrição: Mentoria java");
 	mentoria.setData(LocalDate.now());
 
-	System.out.println(curso1);
-	System.out.println(curso2);
-	System.out.println(mentoria);
+	/*
+	 * System.out.println(curso1);
+	 *  System.out.println(curso2);
+	 * System.out.println(mentoria);
+	 */
+	Bootcamp bootcamp= new Bootcamp();
+	bootcamp.setNome("Bootcamp Java Developer");
+	bootcamp.setDescrição("Bootcamp de desenvolvimento Java");
+	bootcamp.getConteudos().add(curso1);
+	bootcamp.getConteudos().add(curso2);
+	bootcamp.getConteudos().add(mentoria);
+	
+	Dev devCamila= new Dev();
+	devCamila.setNome("Camila");
+	devCamila.inscreverBootcamp(bootcamp);
+	System.out.println("Conteúdos inscritos" + devCamila.getConteudosInscritos());
+	
+	Dev devJoao= new Dev();
+	devJoao.setNome("João");
+	devJoao.inscreverBootcamp(bootcamp);
+	System.out.println("Ce oce tiver problema eu vou te ignorar");
+	System.out.println("Conteúdos inscritos" + devJoao.getConteudosInscritos());
+			
 	}
 
 }
